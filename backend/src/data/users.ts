fd7a618a24160c7d9a33a7d404fd1a9a545d63bc
Erick Maulen <erick.maulen98@gmail.com>
2026-09-08 16:27:37 -0300

    Feat(backend) add mock users dataset and rut/score utils

---
+++ backend/src/data/users.ts
@@ -0,0 +1,25 @@
+import {MockUser} from '../types/index'
+
+export const users: MockUser[] = [
+{
+    id: '1212',
+    username: 'admin',
+    password: 'admin9998',
+    role: 'admin',
+},
+{
+    id: '1213',
+    username: 'user',
+    password: 'user9998',
+    role: 'user',
+    rut: '20183048-6'
+},
+{
+    id: '1214',
+    username: 'user2',
+    password: 'user9998',
+    role: 'user',
+    rut: '19973815-1'
+}
+
+];
